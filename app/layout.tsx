de1e2424bd0e0965ef9ed15a5408637ffa38542d
Wallock <wallock32@gmail.com>
2026-01-16 10:48:22 +0700

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: '7K-AutoCoupon | ระบบแลกคูปองอัตโนมัติ Seven Knights',
-  description: 'ระบบแลกคูปองอัตโนมัติสำหรับเกม Seven Knights - รวดเร็ว สะดวก ง่ายดาย',
-}
+  title: "7K-Tools | ระบบช่วยเหลือ Seven Knights",
+  description:
+    "ระบบแลกคูปองอัตโนมัติ และช่วยเหลืออื่นๆในเกม Seven Knights - รวดเร็ว สะดวก ง่ายดาย ปลอดภัย ไม่เก็บข้อมูลผู้เล่น",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
